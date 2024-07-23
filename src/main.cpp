ea48ee227fd7ca6a50ed7380482fb0ba7c320718
hwthinker <hwthinker@gmail.com>
2024-07-23 15:15:56 +0700

    update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,18 @@
 
 void setup() {
   // Atur kedua pin sebagai output
-  pinMode(LED_PIN1, OUTPUT);
-  pinMode(LED_PIN2, OUTPUT);
+  pinMode(LED_ESP, OUTPUT);
+  pinMode(RLY1, OUTPUT);
 }
 
 void loop() {
   // Nyalakan LED pada pin GPIO2 dan matikan LED pada pin GPIO4
-  digitalWrite(LED_PIN1, HIGH);
-  digitalWrite(LED_PIN2, LOW);
+  digitalWrite(LED_ESP, HIGH);
+  digitalWrite(RLY1, LOW);
   delay(1000); // Tunggu selama 1 detik
 
   // Matikan LED pada pin GPIO2 dan nyalakan LED pada pin GPIO4
-  digitalWrite(LED_PIN1, LOW);
-  digitalWrite(LED_PIN2, HIGH);
+  digitalWrite(LED_ESP, LOW);
+  digitalWrite(RLY1, HIGH);
   delay(1000); // Tunggu selama 1 detik
 }
